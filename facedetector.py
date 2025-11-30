a3c5317715600b36c969828813052464584ae08a
ranvijaygoyal <ranvijaygoyal.work@gmail.com>
2025-11-30 18:27:57 +0530

    Added a counting feature

--- facedetector.py
+++ facedetector.py
@@ -11,9 +11,11 @@ while True:
     faces = face_cascade.detectMultiScale(gray,
     scaleFactor=1.1,
     minNeighbors=5)
-
+    i=1
     for (x, y, w, h) in faces:
+        cv2.putText(img,org=(x-10,y-10),fontScale=0.5,fontFace=cv2.FONT_ITALIC,color=(255,255,0),thickness=1,lineType=cv2.LINE_AA,text=f"FACE {i}")
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,0), 2)
+        i+=1
     cv2.imshow("Faces", img)
     if cv2.waitKey(100)& 0xFF==ord('t'):
         break
